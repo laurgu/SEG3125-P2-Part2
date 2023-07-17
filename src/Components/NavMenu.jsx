da2853f96f14e9fe18ab60ab5ea598f7e285d50f
laurgu <lgu011@uottawa.ca>
2023-07-17 03:38:54 -0400

    setup to host older version of website

--- src/Components/NavMenu.jsx
+++ src/Components/NavMenu.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../Styles/NavMenu.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Styles/NavMenu.css";
 
 function NavMenu() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -17,25 +17,64 @@ function NavMenu() {
         <span className="line"></span>
       </button>
       {isDropdownOpen && (
-        <div className="dropdown-menu" style={{ display: isDropdownOpen ? 'flex' : 'none' }}>
+        <div
+          className="dropdown-menu"
+          style={{ display: isDropdownOpen ? "flex" : "none" }}
+        >
           <div className="background-color">
             <ul>
-              <li><button className="close-button" onClick={toggleDropdown}>X</button></li>
-              <li className="nav-item"><Link to="/">HOME</Link></li>
-              <li><hr /></li>
-              <li className="nav-item"><Link to="/visit">VISIT</Link></li>
-              <li><hr /></li>
-              <li className="nav-item"><Link to="/tickets">TICKETS</Link></li>
-              <li><hr /></li>
-              <li className='subHeading'><Link to="/exhibitions">EXHIBITIONS</Link></li>
-              <li><hr /></li>
-              <li className='subHeading'><Link to="/collection">COLLECTION</Link></li>
-              <li><hr /></li>
-              <li className="nav-item"><Link to="/about">ABOUT</Link></li>
-              <li><hr /></li>
-              <li className="nav-item"><Link to="/contact">CONTACT</Link></li>
-              <li><hr /></li>
-              <li><Link className="red" to="/donate">DONATE</Link></li>
+              <li>
+                <button className="close-button" onClick={toggleDropdown}>
+                  X
+                </button>
+              </li>
+              <li className="nav-item">
+                <Link to="/">HOME</Link>
+              </li>
+              <li>
+                <hr />
+              </li>
+              <li className="nav-item">
+                <Link to="/visit">VISIT</Link>
+              </li>
+              <li>
+                <hr />
+              </li>
+              <li className="nav-item">
+                <Link to="/tickets">TICKETS</Link>
+              </li>
+              <li>
+                <hr />
+              </li>
+              <li className="subHeading">
+                <Link to="/exhibitions">EXHIBITIONS</Link>
+              </li>
+              <li>
+                <hr />
+              </li>
+              <li className="subHeading">
+                <Link to="/collection">COLLECTION</Link>
+              </li>
+              <li>
+                <hr />
+              </li>
+              <li className="nav-item">
+                <Link to="/about">ABOUT</Link>
+              </li>
+              <li>
+                <hr />
+              </li>
+              <li className="nav-item">
+                <Link to="/contact">CONTACT</Link>
+              </li>
+              <li>
+                <hr />
+              </li>
+              <li>
+                <Link className="red" to="/donate">
+                  DONATE
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
